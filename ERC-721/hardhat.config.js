8e79e09b867fe96744bd64541f0bd82513e83aeb
Jumcee <futhmah456@gmail.com>
2024-02-15 17:42:18 +0100

    ERC721

--- ERC-721/hardhat.config.js
+++ ERC-721/hardhat.config.js
@@ -1,6 +1,15 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("@nomicfoundation/hardhat-ethers");
+const { API_URL, PRIVATE_KEY } = process.env;
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: "0.8.17",
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: process.env.API_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+  },
 };
