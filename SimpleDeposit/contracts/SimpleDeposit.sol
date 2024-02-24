b533d41ca65231386f91875ff6f991eaffa32937
Jumcee <futhmah456@gmail.com>
2024-02-24 19:48:24 +0100

    deposit

--- SimpleDeposit/contracts/SimpleDeposit.sol
+++ SimpleDeposit/contracts/SimpleDeposit.sol
@@ -6,14 +6,12 @@ contract SimpleDeposit {
     mapping(address => uint256) public balances; // Stores individual user balances
     uint256 public totalDeposits; // Tracks total funds deposited
 
-    // Deposit funds into the contract
     function deposit() public payable {
         require(msg.value > 0, "Deposit amount must be greater than 0");
         balances[msg.sender] += msg.value;
         totalDeposits += msg.value;
     }
 
-    // Get the balance of a specific address
     function getBalance(address user) public view returns (uint256) {
         return balances[user];
     }
